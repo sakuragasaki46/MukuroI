95b647c31d0605f534adb1c757528999d92eb4bf
Mattia Succurro <matty2000.succ@gmail.com>
2023-04-01 14:43:35 +0200

    more fixes for the buggy ReconnectMixin

--- mukurobot/dbutils.py
+++ mukurobot/dbutils.py
@@ -3,6 +3,7 @@ from peewee import MySQLDatabase
 
 class ReconnectMysqlDatabase(ReconnectMixin, MySQLDatabase):
     _reconnect_errors = ReconnectMixin.reconnect_errors
+    _reconnect_errors += ((0, ''),)
 
 def connect_reconnect(db):
     if db.__class__ == MySQLDatabase:
