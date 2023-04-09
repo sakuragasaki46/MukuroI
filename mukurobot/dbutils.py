3ac1d0b22308ec7e4a485dd87f86fc62b6c73ff6
Mattia Succurro <matty2000.succ@gmail.com>
2023-04-09 10:33:09 +0200

    add peewee async connection fixes (from FastAPI)

--- mukurobot/dbutils.py
+++ mukurobot/dbutils.py
@@ -1,11 +1,32 @@
 from playhouse.shortcuts import ReconnectMixin
-from peewee import MySQLDatabase
+from peewee import MySQLDatabase, _ConnectionState
+from contextvars import ContextVar
 
 class ReconnectMysqlDatabase(ReconnectMixin, MySQLDatabase):
     _reconnect_errors = ReconnectMixin.reconnect_errors
     _reconnect_errors += ((0, ''),)
 
+
+# async peewee helpers (from https://fastapi.tiangolo.com/advanced/sql-databases-peewee/ )
+
+class PeeweeConnectionState(_ConnectionState):
+    def __init__(self, **kwargs):
+        super().__setattr__("_state", db_state)
+        super().__init__(**kwargs)
+
+    def __setattr__(self, name, value):
+        self._state.get()[name] = value
+
+    def __getattr__(self, name):
+        return self._state.get()[name]
+
+db_state_default = {"closed": None, "conn": None, "ctx": None, "transactions": None}
+db_state = ContextVar("db_state", default=db_state_default.copy())
+
 def connect_reconnect(db):
     if db.__class__ == MySQLDatabase:
         db.__class__ = ReconnectMysqlDatabase
+
+    db._state = PeeweeConnectionState()
+    
     return db
